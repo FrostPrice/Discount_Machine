21fb3232d04ee5b4b0c50ca08af3a44d8ffb1073
FrostPrice <barbosamat16@gmail.com>
2020-12-19 23:36:39 -0300

    Add title feature and improved code

--- script.js
+++ script.js
@@ -1,24 +1,51 @@
 "use strict";
 
+/////////////////////////////////////////////////////
+// Variables
+
 const discountContainer = document.querySelector(".total-discounted-container");
-const btnDiscount = document.querySelector(".btn-calculate-discount");
+
+const btnCalcDiscount = document.querySelector(".btn-calculate-discount");
+const btnRandonDiscount = document.querySelector(".btn-randon-discount");
+
 const inputTotal = document.querySelector(".total");
 const inputPercentage = document.querySelector(".percentage");
+
+const title = document.querySelector(".title");
+const labelRandonDiscount = document.querySelector(".label-random-discount");
+
+const overlay = document.querySelector(".overlay");
+const modal = document.querySelector(".modal-discount");
+
+let discount;
+
+/////////////////////////////////////////////////////
+// Functions
 
 const calcPercentage = function (number, discountPerc) {
   const porcentage = discountPerc / 100;
   return number * porcentage;
 };
 
-btnDiscount.addEventListener("click", function (event) {
+const randonDiscount = function () {
+  return Math.round(Math.random() * 99 + 1);
+};
+
+const hideModal = function () {
+  overlay.classList.add("hidden");
+  modal.style.display = "none";
+};
+
+/////////////////////////////////////////////////////
+// Event Listeners
+
+btnCalcDiscount.addEventListener("click", function (event) {
   const inputMoneyValue = Number(inputTotal.value);
   const inputDiscountValue = Number(inputPercentage.value);
 
   event.preventDefault();
 
-  if (isNaN(inputMoneyValue) || isNaN(inputDiscountValue)) {
-    discountContainer.textContent = "Please insert valid number!";
-  } else {
+  if (isFinite(inputMoneyValue) || isFinite(inputDiscountValue)) {
     if (inputDiscountValue >= 1 && inputDiscountValue <= 100) {
       const discount = calcPercentage(inputMoneyValue, inputDiscountValue);
       const total = inputMoneyValue - discount;
@@ -29,7 +56,31 @@ btnDiscount.addEventListener("click", function (event) {
     } else {
       discountContainer.textContent = `Discount percentage is ${inputDiscountValue} and it's not equal or greater than 1`;
     }
+  } else {
+    discountContainer.textContent = "Please insert valid number!";
   }
 });
 
-// Create a new feature for the Title, that when the mouse is over the Title becomes a button to play a game
+btnRandonDiscount.addEventListener("click", function () {
+  labelRandonDiscount.textContent = randonDiscount();
+  discount = labelRandonDiscount.textContent;
+
+  inputPercentage.value = discount;
+});
+
+title.addEventListener("click", function () {
+  overlay.classList.toggle("hidden");
+  modal.classList.toggle("hidden");
+  modal.style.display = "flex";
+});
+
+document.addEventListener("keyup", function (e) {
+  if (e.key === "Escape") {
+    console.log(e.key);
+    hideModal();
+  }
+});
+
+overlay.addEventListener("click", function () {
+  hideModal();
+});
